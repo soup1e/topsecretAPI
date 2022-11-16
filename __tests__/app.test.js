d5f6070c860fc3a5337ec8a1db62fcbac3c35a21
Sam Adams <sam.adams7785@gmail.com>
2022-11-16 15:43:36 -0800

    login and return user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,23 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
 
 const newUser = {
   email: 'admin@test.com',
   password: 'password',
+};
+
+const login = async (userProps = {}) => {
+  const password = userProps.password ?? newUser.password;
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...newUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+
+  return [agent, user];
 };
 
 describe('backend-express-template routes', () => {
@@ -19,6 +32,17 @@ describe('backend-express-template routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
+    });
+  });
+
+  it('login and returns the current user', async () => {
+    const [agent, user] = await login();
+    const me = await agent.get('/api/v1/users/me');
+
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
   afterAll(() => {
