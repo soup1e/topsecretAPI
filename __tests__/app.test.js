48b0b0b3cfbd3a8314d957c85b626d9fe1d9c85f
Sam Adams <sam.adams7785@gmail.com>
2022-11-16 15:18:45 -0800

    setup and create users test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,25 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const newUser = {
+  email: 'admin@test.com',
+  password: 'password',
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(newUser);
+    const { email } = newUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
   afterAll(() => {
     pool.end();
